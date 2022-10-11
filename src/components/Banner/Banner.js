d9e56820fe868b0e55d95b023cb5017695941b15
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-10-11 23:37:19 +0600

    line and bar chart created for total

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import headerBanner from '../../images/headerBanner.jpg'
-import headerBanner2 from '../../images/headerBanner2.jpg'
+import banner from '../../images/banner.jpg'
 
 const Banner = () => {
     return (
         <div className='d-flex align-items-center justify-content-around banner m-5 p-3'>
-            <img src={headerBanner} alt="" className='rounded'/>
-            <img src={headerBanner2} alt="" className='rounded d-none d-lg-block'/>
+            <img src={banner} alt="" className='img-fluid'/>
         </div>
     );
 };
--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,11 +1,61 @@
-import React from 'react';
+import React from "react";
+import { LineChart, Line, XAxis, Tooltip, YAxis, Bar, BarChart, CartesianGrid, Legend } from "recharts";
+import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
-    return (
-        <div>
-            <h1>Statistics</h1>
+  const statisticsData = useLoaderData();
+  const data = statisticsData.data;
+  return (
+    <section className="container">
+      <div className="my-3">
+        <h3 className="highlight ms-5">SimpleLineChart of Total</h3>
+        <div className="my-5">
+          <LineChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <YAxis />
+            <XAxis dataKey="name" />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="total" stroke="#7c4dff" />
+          </LineChart>
         </div>
-    );
+      </div>
+
+      <div className="my-3">
+        <h3 className="highlight ms-5">SimpleBarChart of Total</h3>
+        <div className="my-5">
+        <BarChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="total" fill="#8884d8" />
+        </BarChart>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Statistics;

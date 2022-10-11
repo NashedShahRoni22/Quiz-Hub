d801cd1fb97bfdb88be010c67b41257846b2c356
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-10-11 18:33:45 +0600

    correct answer showing part done

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,23 +1,53 @@
-import React from "react";
-import { Form } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
-import './Question.css'
+import "./Question.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer, toast } from "react-toastify";
 
 const Question = ({ questionData }) => {
-
   const { question, options, correctAnswer } = questionData;
+
+  const notify = () => toast(correctAnswer);
+
+  const handelClick = (option) => {
+    if (option === correctAnswer) {
+      alert("Correct Answer! ");
+    } else {
+      alert("Wrong Answer");
+    }
+  };
   return (
-    <div className="question-card p-3 m-3">
-      <div
-      >
-        <h5 className="my-3 text-center">{question}</h5>
-        <FontAwesomeIcon icon={faEye} className="my-3" size="xl"/>
+    <section className="question-card p-3 m-3">
+      <div>
+        <div>
+          <h5 className="my-3 text-center">{question}</h5>
+        </div>
+        <FontAwesomeIcon
+          icon={faEye}
+          className="my-3"
+          size="xl"
+          onClick={notify}
+        />
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+
         {options.map((option) => (
-          <p className="p-3">{option}</p>
+          <p className="p-3 option" onClick={() => handelClick(option)}>
+            {option}
+          </p>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 

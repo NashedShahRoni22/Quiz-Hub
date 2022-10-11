61e9ba133ed68204b7c6ff8fe4c4d3ff3decf145
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-10-12 01:49:24 +0600

    home navigation part done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,34 @@
 import React from "react";
-import { Container, Nav, Navbar} from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import logo from "../../images/logo.png";
-import './Header.css'
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
       <Navbar bg="light" expand="md">
         <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo} alt="logo" height="80"/>
+          <Navbar.Brand className="navLink">
+            <NavLink to="/">
+              <img src={logo} alt="logo" height="80" />
+              <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>
+            </NavLink>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto textLight navLink">
-              <NavLink to="/quizhub" className="ms-3">QuizHub</NavLink>
-              <NavLink to="/statistic" className="ms-3">Statistic</NavLink>
-              <NavLink to="/blog" className="ms-3">Blog</NavLink>
+            <Nav className="ms-auto navLink">
+              <NavLink to="/quizhub" className="ms-3">
+                QuizHub
+              </NavLink>
+              <NavLink to="/statistic" className="ms-3">
+                Statistic
+              </NavLink>
+              <NavLink to="/blog" className="ms-3">
+                Blog
+              </NavLink>
             </Nav>
           </Navbar.Collapse>
         </Container>

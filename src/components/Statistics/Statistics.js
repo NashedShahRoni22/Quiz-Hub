415efe490f9521531516f85c8e0a9659ade900ef
NashedShahRoni22 <nashed15-11170@diu.edu.bd>
2022-10-12 17:10:37 +0600

    recharts responsive part done

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,24 +1,33 @@
 import React from "react";
-import { LineChart, Line, XAxis, Tooltip, YAxis, Bar, BarChart, CartesianGrid, Legend } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  Tooltip,
+  YAxis,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 import { useLoaderData } from "react-router-dom";
 
 const Statistics = () => {
   const statisticsData = useLoaderData();
   const data = statisticsData.data;
   return (
-    <section className="container">
+    <section className="container d-lg-flex">
       <div className="my-3">
         <h3 className="highlight">SimpleLineChart of Total</h3>
-        <div className="my-5">
+        <ResponsiveContainer width="100%" height={400}>
           <LineChart
-            width={500}
-            height={400}
             data={data}
             margin={{
-              top: 5,
-              right: 30,
+              top: 20,
+              right: 20,
               left: 20,
-              bottom: 5,
+              bottom: 20,
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
@@ -28,31 +37,29 @@ const Statistics = () => {
             <Legend />
             <Line type="monotone" dataKey="total" stroke="#7c4dff" />
           </LineChart>
-        </div>
+        </ResponsiveContainer>
       </div>
 
       <div className="my-3">
         <h3 className="highlight">SimpleBarChart of Total</h3>
-        <div className="my-5">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="total" fill="#8884d8" />
-        </BarChart>
-        </div>
+        <ResponsiveContainer width="100%" height={400}>
+          <BarChart
+            data={data}
+            margin={{
+              top: 20,
+              right: 20,
+              left: 20,
+              bottom: 20,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="total" fill="#8884d8" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     </section>
   );
